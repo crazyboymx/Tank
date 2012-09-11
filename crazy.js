3e62644520ad4d6bbb0a744cc8801c44a11c4178
crazyboymx <xm.crazyboy@gmail.com>
2012-09-11 13:01:25 +0800

    Update crazy.js

--- crazy.js
+++ crazy.js
@@ -32,22 +32,22 @@ Jx().$package(function(J){
         return (angleGunToTurn + this.getHeading() - this.getGunHeading()) % 360;
     };
     var smartFireStrangth = function(energy, distance) {
-        var str = energy < 5*maxEnergy ? engergy/5 : maxEnergy;
-        if (distance > 200) {
-            str /= 3;
-        }
+        var str = energy < 5*3 ? energy/5 : 3;
+    	if (distance > 200) {
+			str /= 3;
+		}
         else if (distance > 150) {
-            str /= 2;
-        }
+			str /= 2;
+		}
         else if (distance > 100) {
-            str /= 1.5;
-        }
-        return str;
+			str /= 1.5;
+		}
+		return str;
     };
     var getDist = function(pos1, pos2) {
         return Math.sqrt((pos1[0]-pos2[0])*(pos1[0]-pos2[0])+(pos1[1]-pos2[1])*(pos1[1]-pos2[1]));
     };
-    // ¼ÆËãÒ»¸öÏòÁ¿µÄ½Ç¶È
+    // è®¡ç®—ä¸€ä¸ªå‘é‡çš„è§’åº¦
     var getVecAngle = function(vec) {
         if (vec[0] == 0 && vec[1] == 0) {
             return 0;
@@ -63,26 +63,26 @@ Jx().$package(function(J){
             return vec[0] > 0 ? 0 : 180;
         }
     };
-    // ¼ÆËãtank×ªµ½posµãĞèÒª×ª¶¯µÄ½Ç¶È ·¶Î§[0,180]
+    // è®¡ç®—tankè½¬åˆ°posç‚¹éœ€è¦è½¬åŠ¨çš„è§’åº¦ èŒƒå›´[0,180]
     var getTurnAngle = function(obj, pos) {
         var tankPos = obj.getPos();
         var angToPos = getVecAngle([pos[0]-tankPos[0],pos[1]-tankPos[1]]);
         return smartTurn(obj.getHeading()-angToPos);
     };
-    // ¼ÆËã´ÓÒ»¸öµã³ö·¢£¬angle·½Ïò£¬dist¾àÀëµÄµã×ø±ê
+    // è®¡ç®—ä»ä¸€ä¸ªç‚¹å‡ºå‘ï¼Œangleæ–¹å‘ï¼Œdistè·ç¦»çš„ç‚¹åæ ‡
     var calcPos = function(oriPos, angle, dist) {
         angle = angle*Math.PI/180;
         var newPos = [dist*Math.cos(angle)+oriPos[0], -dist*Math.sin(angle)+oriPos[1]];
         //alert("oriPos:"+oriPos.toString()+",angle: "+angle+",dist: "+dist+"new Pos: "+newPos.toString());
         return newPos;
     };
-    // ¸ù¾İÎ»ÖÃ£¬³¯Ïò£¬ÔË¶¯·½ÏòºÍĞı×ª·½Ïò£¬¼ÆËãÔ²ÖÜÔË¶¯µÄÔ²ĞÄÎ»ÖÃ
+    // æ ¹æ®ä½ç½®ï¼Œæœå‘ï¼Œè¿åŠ¨æ–¹å‘å’Œæ—‹è½¬æ–¹å‘ï¼Œè®¡ç®—åœ†å‘¨è¿åŠ¨çš„åœ†å¿ƒä½ç½®
     var calcCenter = function(pos, angle, dir, turnDir, radius) {
         angle = turnDir > 0 ? angle-90*dir : angle+90*dir;
         return calcPos(pos, angle, radius);
     };
-    // angle£ºÔ²ÖÜÔË¶¯×ª¶¯µÄ½Ç¶È
-    // ´ÓÒ»¸öµã³ö·¢£¬ÈÆcenterÎªÔ²ĞÄ£¬rÎª°ë¾¶×öÔ²ÖÜÔË¶¯£¬ÔË¶¯angle½Ç¶È£¬¼ÆËã×îÖÕµÄÎ»ÖÃ¡£
+    // angleï¼šåœ†å‘¨è¿åŠ¨è½¬åŠ¨çš„è§’åº¦
+    // ä»posç‚¹å‡ºå‘ï¼Œç»•centerä¸ºåœ†å¿ƒï¼Œrä¸ºåŠå¾„åšåœ†å‘¨è¿åŠ¨ï¼Œè¿åŠ¨angleè§’åº¦ï¼Œè®¡ç®—æœ€ç»ˆçš„ä½ç½®ã€‚
     var calcCycleMotionPos = function(pos, angle, r, center) {
         var fa = getVecAngle([pos[0]-center[0], pos[1]-center[1]]) - angle;
         return calcPos(center, fa, r);
@@ -116,6 +116,7 @@ Jx().$package(function(J){
             this.size = self.getSize();
             this.r = this.tank.moveSpeed / this.tank.turnSpeed;
             this.validArena = new Rect(this.size[0]*0.8, this.size[1]*0.8, this.arena.w-this.size[0]*1.6, this.arena.h-this.size[0]*1.6);
+            this.bulletSpeed = 12;
             this.stepLengthBase = this.size[1];
             this.stepLengthScale = 3;
             this.stepLengthDelta = this.stepLengthBase;
@@ -125,11 +126,12 @@ Jx().$package(function(J){
             this.dir = 1;
             this.dirContCnt = 0;
             self.setAdjustGunForRobotTurn(true);
-
-            this.lastScanedTime = 0;
             this.nextTurnAngle = 0;
+            
+            this.lastScanedTime = 0;
+            this.lastScanedTank = '';
         },
-        // ¸üĞÂµĞÈËĞÅÏ¢e:ScannedRobotEvent
+        // æ›´æ–°æ•Œäººä¿¡æ¯e:ScannedRobotEvent
         updateEnemy: function(e) {
             self = this.tank;
             var name = e.getName();
@@ -142,14 +144,26 @@ Jx().$package(function(J){
             this.enemy[name].heading = e.getHeading();
             this.enemy[name].speed = e.getSpeed();
             this.enemy[name].lastScanedPos = this.enemy[name].pos;
-            this.enemy[name].lastScenedTime = Date.now();
+            this.enemy[name].lastScanedTime = (new Date()).getTime();
             this.enemy[name].pos = calcPos(self.getPos(), self.getHeading()+this.enemy[name].bearing, this.enemy[name].distance);
+            this.lastScanedTime = this.enemy[name].lastScanedTime;
+            this.lastScanedTank = name;
         },
         optimalTurnAngle: function() {
-            if ((Date.now()-this.lastScanedTime) >= 0.1) {
+            // å¦‚æœæ˜¯0.1ç§’ä¹‹å‰çœ‹åˆ°çš„ï¼Œé‡æ–°æ‰«æ
+            if (((new Date()).getTime()-this.lastScanedTime) >= 100) {
                 var ram = Math.random();
                 var dir = ram > 0.5 ? 1 : -1;
                 this.nextTurnAngle = smartTurn(ram*this.turnBase*this.turnScale+this.turnDelta)*dir;
+            }
+            else { // å¦åˆ™ç›¸å¯¹ä»–åšåœ†å‘¨è¿åŠ¨
+                this.tank.say("æˆ‘è½¬æ­»ä½ ~~");
+                var pos = this.tank.getPos();
+                var ePos = this.enemy[this.lastScanedTank].pos;
+                this.nextTurnAngle = smartTurn(this.tank.getHeading()-(getVecAngle([ePos[0]-pos[0], ePos[1]-pos[1]])+90));
+                if (Math.abs(this.nextTurnAngle) < 10) {
+                    this.nextTurnAngle = this.nextTurnAngle > 0 ? 10 : -10;
+                }
             }
             return 19;
         },
@@ -162,9 +176,45 @@ Jx().$package(function(J){
                 this.dirContCnt += 1;
             }
             if (this.willCollision()) {
-                this.tank.say("¿¿£¬Òª×²Ç½À²£¬ÎÒ×ª~~", "#00ff00");
+                this.tank.say("é ï¼Œè¦æ’å¢™å•¦ï¼Œæˆ‘è½¬~~", "#00ff00");
                 this.reversion();
             }
+        },
+        predictEnemyPos: function(enmy, interval) {
+            var et = this.enemy[enmy];
+            var pos = new Array(2);
+            pos[0] = (et.pos[0] + interval*et.speed*Math.cos(et.heading));
+            pos[1] = (et.pos[1] + interval*et.speed*Math.sin(et.heading));
+            return pos;
+        },
+        predictMyPos: function(interval) {
+            var self = this.tank;
+            var currPos = self.getPos();
+            var heading = self.getHeading();
+            var center = calcCenter(currPos, heading, this.dir, this.nextTurnAngle, this.r);
+            return calcCycleMotionPos(currPos, interval*self.turnSpeed, this.r, center);
+        },
+        // é¢„æµ‹å­å¼¹å°„ä¸­enmyéœ€è¦çš„æ—¶é—´
+        predictHitTime: function(enmy) {
+            var et = this.enemy[enmy];
+            var selfPos = this.tank.getPos();
+            var vec = getVecAngle([et.pos[0]-selfPos[0], et.pos[1]-selfPos[1]]);
+            //alert("dist: "+getDist(et.pos, selfPos));
+            //var s = this.bulletSpeed+et.speed*Math.cos(et.heading-vec);
+            //alert("speed:"+s);
+            return getDist(et.pos, selfPos) / this.bulletSpeed+et.speed*Math.cos(et.heading-vec);
+        },
+        // è®¾ç½®ç‚®å£å¯¹å‡†ç›®æ ‡tankï¼Œè¿”å›éœ€è¦è°ƒæ•´çš„è§’åº¦
+        aimTo: function(enmy) {
+            et = this.enemy[enmy];
+            var t = this.predictHitTime(enmy);
+            //alert('hit time: '+t);
+            var myPPos = this.tank.getPos();
+            //alert("myPPos: "+myPPos.toString());
+            var etPPos = this.predictEnemyPos(enmy, t);
+            //alert("etPPos:"+etPPos.toString());
+            var fAngle = getVecAngle([etPPos[0]-myPPos[0], etPPos[1]-myPPos[1]]);
+            return smartTurn(fAngle-this.tank.getGunHeading());
         },
         willCollision: function() {
             self = this.tank;
@@ -173,7 +223,7 @@ Jx().$package(function(J){
             var center = calcCenter(pos, heading, this.dir, this.nextTurnAngle, this.r);
             var nextPos = calcCycleMotionPos(pos, this.nextTurnAngle, this.r, center);
             var dectPos = [nextPos[0],nextPos[1]];
-            // x·½ÏòÉÏ¿ç¹ıÔ²ĞÄ£¬ÔòÄ¬ÈÏy·½Ïò²»»á¿ç¹ıÔ²ĞÄ
+            // xæ–¹å‘ä¸Šè·¨è¿‡åœ†å¿ƒï¼Œåˆ™é»˜è®¤yæ–¹å‘ä¸ä¼šè·¨è¿‡åœ†å¿ƒ
             if ((pos[0]<center[0] && center[0]<nextPos[0]) || (pos[0]>center[0] && center[0]>nextPos[0])) {
                 if (pos[1] > center[1]) {
                     dectPos[1] = center[1]+this.r;
@@ -182,7 +232,7 @@ Jx().$package(function(J){
                     dectPos[1] = center[1]-this.r;
                 }
             }
-            // y·½ÏòÉÏ¿ç¹ıÔ²ĞÄ£¬ÔòÄ¬ÈÏx·½Ïò²»»á¿ç¹ıÔ²ĞÄ
+            // yæ–¹å‘ä¸Šè·¨è¿‡åœ†å¿ƒï¼Œåˆ™é»˜è®¤xæ–¹å‘ä¸ä¼šè·¨è¿‡åœ†å¿ƒ
             if ((pos[1]<center[1] && center[1]<nextPos[1]) || (pos[1]>center[1] && center[1]>nextPos[1])) {
                 if (pos[0] > center[0]) {
                     dectPos[0] = center[0]+this.r;
@@ -198,8 +248,8 @@ Jx().$package(function(J){
                 if (!this.enemy.hasOwnProperty(p)) {
                     continue;
                 }
-                // Èç¹ûÊÇ0.1ÃëÖ®Ç°¿´µ½µÄ²¢ÇÒĞÂÎ»ÖÃ¾àÀëĞ¡ÓÚsize£¬Ôò»áÅö×²
-                if ((Date.now()-this.enemy[p].lastScenedTime) < 0.1 && getDist(pos, this.enemy[p].pos) < this.size[0]) {
+                // å¦‚æœæ˜¯0.1ç§’ä¹‹å‰çœ‹åˆ°çš„å¹¶ä¸”æ–°ä½ç½®è·ç¦»å°äºsizeï¼Œåˆ™ä¼šç¢°æ’
+                if (((new Date()).getTime()-this.enemy[p].lastScenedTime) < 0.1 && getDist(pos, this.enemy[p].pos) < this.size[0]) {
                     return true;
                 }
             }*/
@@ -208,79 +258,90 @@ Jx().$package(function(J){
         reversion: function() {
             this.dir = -this.dir;
             this.dirContCnt = 0;
-            //this.nextTurnAngle = -this.nextTurnAngle;
         },
         step: function() {
             self = this.tank;
             this.optimalTurnAngle();
             this.optimalDirection();
             self.setTurn(this.nextTurnAngle, this.step.bind(this));
-            self.setGunTurnRight(1000*this.dir);
+            self.setGunTurnRight(1000*this);
             self.setAhead(10000*this.dir);
             self.execute();
+        },
+        willFire: function() {
+            
+        },
+        trackEnemy: function() {
+            
         }
     };
-
+    
     var aiPool = [
         new CommonAI()
     ];
-
+    
     Robot = new J.Class({extend : tank.Robot}, {
-        /**
-        *robotÖ÷Ñ­»·
-        **/	
-        run:function(){
-            this.setUI(tank.ui["grey"]);
+		/**
+		*robotä¸»å¾ªç¯
+		**/	
+		run:function(){
+			this.setUI(tank.ui["grey"]);
             aiPool[currAI].init(this);
             this.loop(function() {
                 aiPool[currAI].step();
             });
-        },
+		},
 
-        /**
-        *¿´µ½ÆäËûrobotµÄ´¦Àí³ÌĞò
-        **/
-        onScannedRobot:function(e){
-            aiPool[currAI].updateEnemy(e);
+		/**
+		*çœ‹åˆ°å…¶ä»–robotçš„å¤„ç†ç¨‹åº
+		**/
+		onScannedRobot:function(e){
+			aiPool[currAI].updateEnemy(e);
+            this.turnGun(aiPool[currAI].aimTo(e.getName()));
+            if (this.getGunHeat() == 0) {
+                this.fire(smartFireStrangth(this.getEnergy(), e.getDistance()));
+            }
+            aiPool[currAI].trackEnemy();
+            this.scan();
+            return;
             var angleToRobot=e.getBearing();
             var angleGunToTurn=getGunAngleToTurn.call(this,angleToRobot);
             var angleToTurn=smartTurn(angleGunToTurn);
-
-            // If it's close enough, fire!
-            if (Math.abs(angleToTurn) <= 3) {
-                this.stopMove();
+            
+			// If it's close enough, fire!
+			if (Math.abs(angleToTurn) <= 3) {
+                //this.stopMove();
                 this.turnGunLeft(angleToTurn);
-                // We check gun heat here, because calling fire()
-                // uses a turn, which could cause us to lose track
-                // of the other robot.
-                if (this.getGunHeat() == 0) {
-                    this.fire(Math.min(3 - Math.abs(angleToTurn), this.getEnergy() - .1));
-                }
-            } // otherwise just set the gun to turn.
-            else {
-                this.stopMove();
-                this.turnGunLeft(angleToTurn);
-            }
-            this.scan();
-        },
+				// We check gun heat here, because calling fire()
+				// uses a turn, which could cause us to lose track
+				// of the other robot.
+				if (this.getGunHeat() == 0) {
+					this.fire(Math.min(3 - Math.abs(angleToTurn), this.getEnergy() - 0.1));
+				}
+			} // otherwise just set the gun to turn.
+			else {
+                //this.stopMove();
+				this.turnGunLeft(angleToTurn);
+			}
+			this.scan();
+		},
 
-        /**
-        *±»×Óµ¯»÷ÖĞµÄ´¦Àí³ÌĞò
-        **/
-        onHitByBullet:function(e){
-            this.say('²»Òª´òÎÒÀ²£¬ÎÒÊÇÎŞ¹¼µÄ~~');
-            this.back(100);
-        },
+		/**
+		*è¢«å­å¼¹å‡»ä¸­çš„å¤„ç†ç¨‹åº
+		**/
+		onHitByBullet:function(e){
+			//this.energy = 100;
+		},
 
-        /**
-        *ºÍÇ½Åö×²µÄ´¦Àí³ÌĞò
-        **/
-        onHitWall:function(e){
+		/**
+		*å’Œå¢™ç¢°æ’çš„å¤„ç†ç¨‹åº
+		**/
+		onHitWall:function(e){
+			this.say("nnd,è¿˜æ˜¯æ’å¢™äº†ï¼ï¼ï¼", "#ff0000");
+		},
 
-        },
-
-        onRobotDeath:function(e){
-
-        }
-    });
+		onRobotDeath:function(e){
+			
+		}
+	});
 });
